a06b7a89bda010ac1f122b876aa724e12d2208a2
moaeydsede <moaeydsede98@gmail.com>
2026-05-11 11:43:11 +0300

    Delete src directory

--- src/components/Brand.tsx
+++
@@ -1,8 +0,0 @@
-export default function Brand({ compact = false }: { compact?: boolean }) {
-  return (
-    <div className="brand">
-      <div className="brand-mark">م</div>
-      {!compact && <div><strong>عيادات المشفى</strong><span>نظام الحجوزات ومتابعة الدور</span></div>}
-    </div>
-  )
-}
--- src/pages/NotFound.tsx
+++
@@ -1,2 +0,0 @@
-import { Link } from 'react-router-dom'
-export default function NotFound() { return <section className="not-found"><h1>404</h1><p>الصفحة غير موجودة</p><Link className="primary-btn" to="/">العودة للرئيسية</Link></section> }
