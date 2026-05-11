125a6d56ca311ced391dbec87b72b33fe45513db
moaeydsede <moaeydsede98@gmail.com>
2026-05-11 11:13:12 +0300

    Delete src directory

--- src/data/seed.ts
+++
@@ -1,6 +0,0 @@
-import type { ActivityLog, Booking, Doctor, Patient } from '../types'
-
-export const seedDoctors: Doctor[] = []
-export const seedPatients: Patient[] = []
-export const seedBookings: Booking[] = []
-export const seedLogs: ActivityLog[] = []
